7ff27d227ac62095126fc5fa38cb300f38e6970a
ParthX05 <theparth05@gmail.com>
2025-06-18 01:49:15 +0530

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,15 @@
+package employees;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/studentdb", "root", "ParthMehta_05");
+        } catch (Exception e) {
+            System.out.println("DB Error: " + e.getMessage());
+        }
+        return con;
+    }
+}
